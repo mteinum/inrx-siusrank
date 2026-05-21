079b530e04de5fc45201ae60fa435e1c74aee2e6
Morten Teinum <morten.teinum@telia.no>
2026-05-21 07:37:54 +0200

    Forenkle NM pistol shoot events

--- InrxToSiusRank/tests/InrxToSiusRank.Tests/ShooterGroupsTemplateTests.cs
+++ InrxToSiusRank/tests/InrxToSiusRank.Tests/ShooterGroupsTemplateTests.cs
@@ -1,5 +1,7 @@
 namespace InrxToSiusRank.Tests;
 
+using System.Xml.Linq;
+
 public sealed class ShooterGroupsTemplateTests
 {
     [Fact]
@@ -21,6 +23,64 @@ public sealed class ShooterGroupsTemplateTests
 
         Assert.True(template.TryGet("Apen", out var group));
         Assert.Equal(106, group.Index);
+    }
+
+    [Fact]
+    public void Embedded_shooter_group_template_contains_standard_and_nm_25_50m_pistol_classes()
+    {
+        var template = ShooterGroupsTemplate.Load(Path.Combine(TemplatesDirectory(), "ShooterGroupsTemplate.xml"));
+
+        var groupNames = template.Groups.Select(group => group.Name).ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+        Assert.True(
+            new[]
+            {
+                "Regulars", "MQS", "SH1", "SH2", "Guests", "Junior", "RPO", "JuniorRecord",
+                "GP", "GPMQS", "Substitutes", "Youth", "YouthRecord", "Precision", "Sporter"
+            }.All(groupNames.Contains));
+
+        Assert.True(
+            new[] { "Apen", "Menn", "Kvinner", "Jr", "Jrm", "Jrk", "U", "V55", "V65", "V73" }
+                .All(groupNames.Contains));
+    }
+
+    [Fact]
+    public void Embedded_shoot_events_have_default_groups_from_shooter_group_template()
+    {
+        var templatesDirectory = TemplatesDirectory();
+        var shooterGroupsTemplate = ShooterGroupsTemplate.Load(Path.Combine(templatesDirectory, "ShooterGroupsTemplate.xml"));
+        var document = XDocument.Load(Path.Combine(templatesDirectory, "ShootEventsTemplate2026_NM_Pistol.xml"));
+
+        var events = document.Root!
+            .Elements("ShootEventConfiguration")
+            .Select(element => new
+            {
+                Name = element.Element("Name")!.Value,
+                DefaultGroup = element.Element("DefaultShooterGroup")!.Value
+            })
+            .ToArray();
+
+        Assert.Equal(11, events.Length);
+        Assert.All(events, shootEvent => Assert.True(
+            shooterGroupsTemplate.TryGet(shootEvent.DefaultGroup, out _),
+            $"{shootEvent.Name} has unknown default group {shootEvent.DefaultGroup}."));
+    }
+
+    private static string TemplatesDirectory()
+    {
+        var directory = new DirectoryInfo(AppContext.BaseDirectory);
+        while (directory is not null)
+        {
+            var candidate = Path.Combine(directory.FullName, "src", "InrxToSiusRank", "Templates");
+            if (Directory.Exists(candidate))
+            {
+                return candidate;
+            }
+
+            directory = directory.Parent;
+        }
+
+        throw new DirectoryNotFoundException("Could not find src/InrxToSiusRank/Templates.");
     }
 
     private sealed class TempFile : IDisposable
